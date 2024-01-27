b48bcac12c2949befd5728a900d3d20db616ff33
Jagadeesan J <jagadeesanj@outlook.com>
2024-01-27 09:17:09 +0530

    Project Upload

---
+++ Data/MvcDemoDbContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using WebCore.Models.Domain;
+
+namespace WebCore.Data
+{
+    public class MvcDemoDbContext : DbContext
+    {
+        public MvcDemoDbContext(DbContextOptions options) : base(options)
+        {
+        }
+
+
+        public DbSet<Employee> Employees { get; set; }
+    }
+}
